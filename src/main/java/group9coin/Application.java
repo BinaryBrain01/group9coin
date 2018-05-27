7faf21863fd2b3e05089c68ede80431e0bb036ba
jolandaduinhoven <jolandaduinhoven@gmail.com>
2018-05-27 10:52:07 +0200

    Write found block to database

--- src/main/java/group9coin/Application.java
+++ src/main/java/group9coin/Application.java
@@ -7,10 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     @Autowired
@@ -42,9 +41,6 @@ public class Application {
             count++;
         }
     }
-
-
-
 
 
 }
--- src/main/java/group9coin/db/BlockRepository.java
+++ src/main/java/group9coin/db/BlockRepository.java
@@ -1,9 +1,7 @@
 package group9coin.db;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BlockRepository extends CrudRepository<DbBlock, Long> {
 
 }
